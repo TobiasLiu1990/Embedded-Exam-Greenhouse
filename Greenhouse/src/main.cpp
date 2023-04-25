2ff9ab21c49e874d59ba4d136f3ce08ccbd70af3
TobiasLiu1990 <tobias.liu@live.se>
2023-04-26 00:48:31 +0200

    Extracted http + Serial.println to methods

--- Greenhouse/src/main.cpp
+++ Greenhouse/src/main.cpp
@@ -54,77 +54,86 @@ const String metric = "&units=metric";
 
 StaticJsonDocument<1024> doc;
 
+// Forward declarations
+void GetWeatherInfo();
+void WeatherInfoToSerial(JsonObject weatherInfo, JsonObject mainInfo);
+
+
 void setup() {
-  Serial.begin(115200);
-  delay(2000);
+    Serial.begin(115200);
+    delay(2000);
 
-  WiFi.begin(ssid, password);
+    WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to Wifi...");
-  }
-  Serial.println("Connected to the Wifi network");
-  Serial.println(WiFi.localIP());
-
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to Wifi...");
+    }
+    Serial.println("Connected to the Wifi network");
+    Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
 
-    http.begin(endpoint + key + metric);
-    int httpCode = http.GET();
-
-    // https://github.com/espressif/arduino-esp32/blob/master/libraries/HTTPClient/src/HTTPClient.h#L36
-    if (httpCode > 0) {     //Code > 0 are standard HTTP return codes
-      String payload = http.getString();
-
-      DeserializationError error = deserializeJson(doc, payload);     // parses a JSON input and puts the result in a JsonDocument.
-      if (error) {
-        Serial.print(F("deserializeJson() failed"));
-        Serial.println(error.f_str());                //Same as c_str(), except the string is in Flash memory (only relevant for AVR and ESP8266)
-      }
-
-      //Json
-      JsonObject weather_0 = doc["weather"][0];
-      const char* weather_0_desc = weather_0["description"];
-
-      JsonObject mainInfo = doc["main"];
-      float mainInfo_temp = mainInfo["temp"];
-      float mainInfo_minTemp = mainInfo["temp_min"];
-      float mainInfo_maxTemp = mainInfo["temp_max"];
-      int mainInfo_humidity = mainInfo["humidity"];
-
-
-      Serial.println("Weather status: ");
-      Serial.print("Current weather: ");
-      Serial.println(weather_0_desc);
-
-      Serial.print("Temperature: ");
-      Serial.println(mainInfo_temp);
-
-      Serial.print("Lowest temperature today: ");
-      Serial.println(mainInfo_minTemp);
-
-      Serial.print("Highest temperature today: ");
-      Serial.println(mainInfo_maxTemp);
-
-      Serial.print("Humidity %: ");
-      Serial.println(mainInfo_humidity);
-
-
-    } else {
-      Serial.println("Error on HTTP request");
-    }
-    http.end();
-  }
-  delay(600000);
-
-  Serial.println("test test");
+    // Openweathermap.org API for weather info
+    GetWeatherInfo();
 }
 
+void GetWeatherInfo() {
+    if (WiFi.status() == WL_CONNECTED) {
+        HTTPClient http;
+
+        http.begin(endpoint + key + metric);
+        int httpCode = http.GET();
+
+        // https://github.com/espressif/arduino-esp32/blob/master/libraries/HTTPClient/src/HTTPClient.h#L36
+        if (httpCode > 0) { // Code > 0 are standard HTTP return codes
+            String payload = http.getString();
+
+            DeserializationError error = deserializeJson(doc, payload); // parses a JSON input and puts the result in a JsonDocument.
+            if (error) {
+                Serial.print(F("deserializeJson() failed"));
+                Serial.println(error.f_str()); // Same as c_str(), except the string is in Flash memory (only relevant for AVR and ESP8266)
+            }
+
+            // Json
+            JsonObject weather_0 = doc["weather"][0];
+            JsonObject mainInfo = doc["main"];
+            WeatherInfoToSerial(weather_0, mainInfo);
+        } else {
+            Serial.println("Error on HTTP request");
+        }
+        http.end();
+    }
+    delay(5000);
+}
+
+// Temp method for printing info to Serial
+void WeatherInfoToSerial(JsonObject weatherInfo, JsonObject mainInfo) {
+    const char *weatherDescription = weatherInfo["description"];
+
+    float mainInfo_temp = mainInfo["temp"];
+    float mainInfo_minTemp = mainInfo["temp_min"];
+    float mainInfo_maxTemp = mainInfo["temp_max"];
+    int mainInfo_humidity = mainInfo["humidity"];
+
+    Serial.println("Weather status: ");
+    Serial.print("Current weather: ");
+    Serial.println(weatherDescription);
+
+    Serial.print("Temperature: ");
+    Serial.println(mainInfo_temp);
+
+    Serial.print("Lowest temperature today: ");
+    Serial.println(mainInfo_minTemp);
+
+    Serial.print("Highest temperature today: ");
+    Serial.println(mainInfo_maxTemp);
+
+    Serial.print("Humidity %: ");
+    Serial.println(mainInfo_humidity);
+}
 
 void GetTime() {
-  //currentDate = new Date();
+    // currentDate = new Date();
 }
