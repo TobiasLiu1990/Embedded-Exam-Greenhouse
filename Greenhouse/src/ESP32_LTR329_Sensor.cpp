5b3de6be7b14505ad03884d533ff495a310e93ef
TobiasLiu1990 <tobias.liu@live.se>
2023-05-03 01:02:57 +0200

    Some changes in LTR329 .cpp .h

--- Greenhouse/src/ESP32_LTR329_Sensor.cpp
+++ Greenhouse/src/ESP32_LTR329_Sensor.cpp
@@ -14,7 +14,6 @@ unsigned int ESP32_LTR329_Sensor::getFromLightSensor(unsigned int lux) {
         // 1st param = ch0
         // 2nd param = ch1. Reads both 16-bit channels at once
         bool data = ltr329.readBothChannels(visibleAndIr, infrared);
-
         if (data) {
             if (visibleAndIr > 65535 || infrared > 65535) { // Saturated sensors
                 return 0;
@@ -24,12 +23,21 @@ unsigned int ESP32_LTR329_Sensor::getFromLightSensor(unsigned int lux) {
             }
 
             double ratio = infrared / (visibleAndIr + infrared);
+            Serial.println();
+            Serial.print("ch0: ");
+            Serial.println(visibleAndIr);
+            Serial.print("ch1: ");
+            Serial.print(infrared);
+            Serial.println();
 
             if (ratio < 0.45) {
+                Serial.println("ratio 1");
                 return ((1.7743 * visibleAndIr) + (1.1059 * infrared));
             } else if (ratio < 0.64 && ratio >= 0.45) {
+                Serial.println("ratio 2");
                 return ((4.2785 * visibleAndIr) - (1.9548 * infrared));
             } else if (ratio < 0.85 && ratio >= 0.64) {
+                Serial.println("ratio 3");
                 return ((0.5926 * visibleAndIr) + (0.1185 * infrared));
             } else {
                 return 0;
